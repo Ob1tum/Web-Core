9db4b5d4c21bdb53660a433764f68acd1aea57e8
Ob1tum <Obitum@yandex.ru>
2022-05-26 20:48:38 +0300

    Update script

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function readMore() {
     let hiddenSlider = document.getElementById("hidden-slider");
     let showMore = document.getElementById("show-more");
-    if(hiddenSlider.style.display === "none") {
+    
+    if(hiddenSlider.style.display === "flex") {
+        hiddenSlider.style.display = "none";
+        showMore.innerHTML = "Показать всё";
+        showMore.style.backgroundImage = "url(../images/labelicon.svg)";
+    } else {
         hiddenSlider.style.display = "flex";
         showMore.innerHTML = "Скрыть";
         showMore.style.backgroundImage = "url(../images/labeliconreverse.svg)";
-    } else {
-        hiddenSlider.style.display = "none";
-        showMore.innerHTML = "Показать всё";
-        showMore.style.backgroundImage = "url(../images/labelicon.svg)"
     }
 }
