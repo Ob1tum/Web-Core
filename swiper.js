f5e0ca68afb120f07fda02858f78b4f2e9e6506d
Ob1tum <Obitum@yandex.ru>
2022-06-10 13:01:37 +0300

    desctop

--- swiper.js
+++ swiper.js
@@ -1,7 +1,12 @@
-new Swiper ('.swiper',{
+var swiper = new Swiper ('.swiper',{
     pagination: {
         el: '.swiper-pagination',
     },
 });
 
+var swiperTech = new Swiper ('.swiper2',{
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
 
